995351af79e5433bb3f37afaf6bd92ea5acec6b5
p152857 <71158607+p152857@users.noreply.github.com>
2026-05-14 05:59:33 +0300

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// إعدادات منصة فرسان ابن الوليد
-window.FORSAAN_CONFIG = {
-  RESULTS_API_URL: 'https://script.google.com/macros/s/AKfycbwGV7fPD8ewaulz0pXGbLa9idyEFY-647797rR8ijG97jooJkG7k-fh-KTHJWmBkb5_Dg/exec',
-  TEACHER_CODE: 'Rayan9892026@@@',
-  REFRESH_SECONDS: 30
-};

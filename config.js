03b9f599b1155ad5ad432a4201f35655e0a2efec
p152857 <71158607+p152857@users.noreply.github.com>
2026-05-14 12:06:44 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // إعدادات منصة فرسان ابن الوليد
 window.FORSAAN_CONFIG = {
-  RESULTS_API_URL: 'https://script.google.com/macros/s/AKfycbwXeieRpSrOoqOe7j01Qk3zI-BqIWSWEn7DeB6YiUMLPqZWVxe2kKerxIu0YhhXLOHpVw/exec',
+  RESULTS_API_URL: 'https://script.google.com/macros/s/AKfycbz_mAYubzqHlXr8dGev7l6ODRDKIBKx7um2WTd251gz0bSCrMYz-e9ZJPHqa8FgRZS1JQ/exec',
   TEACHER_CODE: 'Rayan9892026@@@',
   REFRESH_SECONDS: 30
 };
